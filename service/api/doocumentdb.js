12d787d0ff547c3cf8c5f614b21dc6ca556c20d2
windowsazure <windowsazure>
2015-03-11 20:39:29 +0000

    Committing update from request /api/scmvfs/service/api/doocumentdb.js

--- service/api/doocumentdb.js
+++ service/api/doocumentdb.js
@@ -8,7 +8,7 @@ exports.post = function(request, response) {
 
 exports.get = function(request, response) {
     var http=require('http')
-    var port=process.env.port||13337;
+    var port=process.env.port||1337;
     http.createServer(function(req,res){
         res.writeHead(200,{'Content-Type':'text/plain'})
         res.end('Hello World\n')
