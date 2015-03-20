964ae2f1f5482c55fbfa692749a16d72c23e3fd3
windowsazure <windowsazure>
2015-03-20 23:41:25 +0000

    Committing update from request /api/scmvfs/service/api/doocumentdb.js

--- service/api/doocumentdb.js
+++ service/api/doocumentdb.js
@@ -11,5 +11,6 @@ exports.get = function(request, response) {
     var DocumentClient=require("documentdb").DocumentClient
     var endpoint="https://captacion.documents.azure.com:443/"
     var authKey="3/DEbMcpayYBWuqzwCNgtdCx6IFHm8hew5Ev/20m6ZkEq1YNV8QAzQ40SWrMyXLI4nmLYZn1RxkVy4Tvg6rPJg=="
+    var client=new DocumentClient(endpoint,{"masterKey":authKey})
     response.send(statusCodes.OK, { message :'hola'});
 };
